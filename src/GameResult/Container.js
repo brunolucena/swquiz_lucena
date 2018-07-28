b44c274f13f7fa4ad5712d8848f4b92b09188525
Bruno Lucena <bruno.lucena@wide.net.br>
2018-07-28 17:36:24 -0300

    Adicionar validação de formulário GameResult

--- src/GameResult/Container.js
+++ src/GameResult/Container.js
@@ -5,30 +5,122 @@ import PropTypes from 'prop-types';
 const Container = (Component) => (
 	class extends React.Component {
 		static propTypes = {
-			email: PropTypes.string, // Email of player if game already has been saved
+			hasResult: PropTypes.bool.isRequired, // Game has a result
 			loading: PropTypes.bool, // Is result beeing
-			name: PropTypes.string, // Name of player if game already has been saved
+			loadingMessage: PropTypes.string, // Message to display while loading
+			playerEmail: PropTypes.string, // Email of player if game already has been saved
+			playerName: PropTypes.string, // Name of player if game already has been saved
 			saveGameData: PropTypes.func.isRequired, // Function to save game data
 			score: PropTypes.number // Game score
+		};
+
+		static defaultProps = {
+			loadingMessage: 'Calculando pontuação...'
 		};
 
 		constructor(props) {
 			super(props);
 
 			this.state = {
-				emailInput: props.email,
-				nameInput: props.name
+				email: '',
+				formErrors: {email: '', name: ''},
+				isFormValid: false,
+				name: ''
 			};
 
 			this.handleInputChange = this.handleInputChange.bind(this);
+			this.saveGameDataHandler = this.saveGameDataHandler.bind(this);
 		}
 
+		/**
+		 * @description Changes input text and verifies if it's valid.
+		 *
+		 * @param {object} event Input
+		 */
 		handleInputChange(event) {
 			event.preventDefault();
 
+			const { formErrors } = this.state;
+
+			const name = event.target.name;
+			const value = event.target.value;
+
+			const validator = this.validateField(name, value);
+
+			if (!validator.valid) {
+				formErrors[name] = validator.message;
+			} else {
+				formErrors[name] = '';
+			}
+
 			this.setState({
-				[event.target.name]: event.target.value
+				formErrors,
+				[name]: value
 			});
+		}
+
+		saveGameDataHandler(event) {
+			event.preventDefault();
+
+			console.log('saveGameDataHandler', this.validateForm());
+		}
+
+		validateField(fieldName, value) {
+			const validator = {
+				valid: true,
+				message: ''
+			};
+
+			switch (fieldName) {
+				case 'email':
+    				const emailFilter = /^.+@.+\..{2,}$/;
+    				const illegalChars = /[\(\)\<\>\,\;\:\\\/\"\[\]]/
+
+					const isEmailValid = emailFilter.test(value) || value.match(illegalChars);
+
+					validator.valid = isEmailValid;
+					validator.message = isEmailValid ? '' : ' Invalid email';
+					break;
+				case 'name':
+					const isNameValid = value.length > 0;
+
+					validator.valid = isNameValid;
+					validator.message = isNameValid ? '' : " Required";
+					break;
+				default:
+					break;
+		  	}
+
+		  	return validator
+		}
+
+		validateForm() {
+			const { formErrors } = this.state;
+
+			Object.keys(formErrors).forEach(fieldName => {
+				const value = formErrors[fieldName];
+
+				const validator = this.validateField(fieldName, this.state[fieldName]);
+
+				if (!validator.valid) {
+					formErrors[fieldName] = validator.message;
+				} else {
+					formErrors[fieldName] = '';
+				}
+			});
+
+			const errorsCount = Object.values(formErrors).filter(error => error.length > 1).length;
+			let isFormValid = true;
+
+			if (errorsCount > 0) {
+				isFormValid = false;
+			}
+
+			this.setState({
+				formErrors
+			});
+
+			return isFormValid
 		}
 
 		render() {
@@ -37,6 +129,7 @@ const Container = (Component) => (
 					{...this.props}
 					{...this.state}
 					handleInputChange={this.handleInputChange}
+					saveGameData={this.saveGameDataHandler}
 				/>
 			)
 		}
--- src/GameResult/index.js
+++ src/GameResult/index.js
@@ -2,17 +2,82 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import CircularProgress from '@material-ui/core/CircularProgress';
+import TextField from '@material-ui/core/TextField';
 
 import Container from './Container'
 import styles from './styles.scss';
 
 
 const GameResult = (props) => {
+	const { email, handleInputChange, hasResult, formErrors, loading, loadingMessage,
+			name, playerEmail, playerName, saveGameData, score } = props;
+
 	return (
 		<div className={styles.gameResultWrapper}>
-			GameResult
+			<div className={styles.top}>
+				Quiz ended!
+			</div>
+
+			<div className={styles.gameData}>
+				<div className={styles.score}>
+					<span>{score}</span> points
+				</div>
+
+				<div className={styles.text}>
+					Fill the form below to save your score:
+				</div>
+				{
+				// !loading ?
+				// 	[
+				// 		<CircularProgress key="circular-progress" style={{color: "#b9ab25"}}/>,
+				// 		<div key="info" className={styles.info}>
+				// 			{loadingMessage}
+				// 		</div>
+				// 	]
+				// :
+						// {
+							!hasResult &&
+								<div className={styles.inputsContainer}>
+									<TextField
+										{...(formErrors.name && {error: !!formErrors.name})}
+										className={styles.textField}
+										helperText={formErrors.name}
+										label="Name"
+										name="name"
+										onChange={handleInputChange}
+										value={name}
+									/>
+									<TextField
+										{...(formErrors.email && {error: !!formErrors.email})}
+										className={styles.textField}
+										helperText={formErrors.email}
+										label="Email"
+										name="email"
+										onChange={handleInputChange}
+										value={email}
+									/>
+
+								</div>
+						// }
+				}
+
+				<button className={styles.button} onClick={saveGameData}>Save</button>
+			</div>
 		</div>
 	);
+}
+
+GameResult.propTypes = {
+	email: PropTypes.string, // Email input if data has not been saved yet
+	handleInputChange: PropTypes.func.isRequired, // Function to change input text
+	hasResult: PropTypes.bool.isRequired, // Game has a result
+	loading: PropTypes.bool.isRequired, // Is result being loaded
+	loadingMessage: PropTypes.string.isRequired, // Message to display while loading
+	name: PropTypes.string, // Name input if data has not been saved yet
+	playerEmail: PropTypes.string, // Player email if game has already been saved
+	playerName: PropTypes.string, // Player name if game has already been saved
+	saveGameData: PropTypes.func.isRequired, // Function to save game data
+	score: PropTypes.number // Game score
 }
 
 export default Container(GameResult);
