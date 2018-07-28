158f1e5d0dc18afac90cceee7df03d19fbcb3976
Bruno Lucena <bruno.lucena@wide.net.br>
2018-07-28 00:38:37 -0300

    Ajustar CharInfo para exibir dados

--- src/CharInfo/index.js
+++ src/CharInfo/index.js
@@ -5,9 +5,41 @@ import styles from './styles.scss';
 
 
 const CharInfo = (props) => {
+	const { birth_year, eye_color, films, gender, hair_color, height,
+			homeworld, imageUrl, mass, skin_color, species, starships, vehicles
+	} = props;
+
 	return (
-		<div>
-			CharInfo
+		<div className={styles.charInfoWrapper}>
+			birth_year {birth_year}
+			eye_color {eye_color}
+			films {
+				films.map(film => (
+					film.title
+				))
+			}
+			gender {gender}
+			hair_color {hair_color}
+			height {height}
+			homeworld {homeworld.name}
+			imageUrl {imageUrl}
+			mass {mass}
+			skin_color {skin_color}
+			species {
+				species.map(specie => (
+					specie.name
+				))
+			}
+			starships {
+				starships.map(starship => (
+					starship.name
+				))
+			}
+			vehicles {
+				vehicles.map(vehicle => (
+					vehicle.name
+				))
+			}
 		</div>
 	);
 }
