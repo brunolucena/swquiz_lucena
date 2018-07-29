ebccc6f2bd192f0c840da20bd09e86562cdf6f55
Bruno Lucena <bruno.lucena@wide.net.br>
2018-07-29 10:04:41 -0300

    Adicionar imagens

---
+++ src/images/index.js
@@ -0,0 +1,183 @@
+function importAll(r) {
+  return r.keys().map(r);
+}
+
+const folder1 = importAll(require.context('./1', true, /\.(png|jpe?g|svg)$/));
+const folder2 = importAll(require.context('./2', true, /\.(png|jpe?g|svg)$/));
+const folder3 = importAll(require.context('./3', true, /\.(png|jpe?g|svg)$/));
+const folder4 = importAll(require.context('./4', true, /\.(png|jpe?g|svg)$/));
+const folder5 = importAll(require.context('./5', true, /\.(png|jpe?g|svg)$/));
+const folder6 = importAll(require.context('./6', true, /\.(png|jpe?g|svg)$/));
+const folder7 = importAll(require.context('./7', true, /\.(png|jpe?g|svg)$/));
+const folder8 = importAll(require.context('./8', true, /\.(png|jpe?g|svg)$/));
+const folder9 = importAll(require.context('./9', true, /\.(png|jpe?g|svg)$/));
+const folder10 = importAll(require.context('./10', true, /\.(png|jpe?g|svg)$/));
+const folder11 = importAll(require.context('./11', true, /\.(png|jpe?g|svg)$/));
+const folder12 = importAll(require.context('./12', true, /\.(png|jpe?g|svg)$/));
+const folder13 = importAll(require.context('./13', true, /\.(png|jpe?g|svg)$/));
+const folder14 = importAll(require.context('./14', true, /\.(png|jpe?g|svg)$/));
+const folder15 = importAll(require.context('./15', true, /\.(png|jpe?g|svg)$/));
+const folder16 = importAll(require.context('./16', true, /\.(png|jpe?g|svg)$/));
+const folder17 = importAll(require.context('./17', true, /\.(png|jpe?g|svg)$/));
+const folder18 = importAll(require.context('./18', true, /\.(png|jpe?g|svg)$/));
+const folder19 = importAll(require.context('./19', true, /\.(png|jpe?g|svg)$/));
+const folder20 = importAll(require.context('./20', true, /\.(png|jpe?g|svg)$/));
+const folder21 = importAll(require.context('./21', true, /\.(png|jpe?g|svg)$/));
+const folder22 = importAll(require.context('./22', true, /\.(png|jpe?g|svg)$/));
+const folder23 = importAll(require.context('./23', true, /\.(png|jpe?g|svg)$/));
+const folder24 = importAll(require.context('./24', true, /\.(png|jpe?g|svg)$/));
+const folder25 = importAll(require.context('./25', true, /\.(png|jpe?g|svg)$/));
+const folder26 = importAll(require.context('./26', true, /\.(png|jpe?g|svg)$/));
+const folder27 = importAll(require.context('./27', true, /\.(png|jpe?g|svg)$/));
+const folder28 = importAll(require.context('./28', true, /\.(png|jpe?g|svg)$/));
+const folder29 = importAll(require.context('./29', true, /\.(png|jpe?g|svg)$/));
+const folder30 = importAll(require.context('./30', true, /\.(png|jpe?g|svg)$/));
+const folder31 = importAll(require.context('./31', true, /\.(png|jpe?g|svg)$/));
+const folder32 = importAll(require.context('./32', true, /\.(png|jpe?g|svg)$/));
+const folder33 = importAll(require.context('./33', true, /\.(png|jpe?g|svg)$/));
+const folder34 = importAll(require.context('./34', true, /\.(png|jpe?g|svg)$/));
+const folder35 = importAll(require.context('./35', true, /\.(png|jpe?g|svg)$/));
+const folder36 = importAll(require.context('./36', true, /\.(png|jpe?g|svg)$/));
+const folder37 = importAll(require.context('./37', true, /\.(png|jpe?g|svg)$/));
+const folder38 = importAll(require.context('./38', true, /\.(png|jpe?g|svg)$/));
+const folder39 = importAll(require.context('./39', true, /\.(png|jpe?g|svg)$/));
+const folder40 = importAll(require.context('./40', true, /\.(png|jpe?g|svg)$/));
+const folder41 = importAll(require.context('./41', true, /\.(png|jpe?g|svg)$/));
+const folder42 = importAll(require.context('./42', true, /\.(png|jpe?g|svg)$/));
+const folder43 = importAll(require.context('./43', true, /\.(png|jpe?g|svg)$/));
+const folder44 = importAll(require.context('./44', true, /\.(png|jpe?g|svg)$/));
+const folder45 = importAll(require.context('./45', true, /\.(png|jpe?g|svg)$/));
+const folder46 = importAll(require.context('./46', true, /\.(png|jpe?g|svg)$/));
+const folder47 = importAll(require.context('./47', true, /\.(png|jpe?g|svg)$/));
+const folder48 = importAll(require.context('./48', true, /\.(png|jpe?g|svg)$/));
+const folder49 = importAll(require.context('./49', true, /\.(png|jpe?g|svg)$/));
+const folder50 = importAll(require.context('./50', true, /\.(png|jpe?g|svg)$/));
+const folder51 = importAll(require.context('./51', true, /\.(png|jpe?g|svg)$/));
+const folder52 = importAll(require.context('./52', true, /\.(png|jpe?g|svg)$/));
+const folder53 = importAll(require.context('./53', true, /\.(png|jpe?g|svg)$/));
+const folder54 = importAll(require.context('./54', true, /\.(png|jpe?g|svg)$/));
+const folder55 = importAll(require.context('./55', true, /\.(png|jpe?g|svg)$/));
+const folder56 = importAll(require.context('./56', true, /\.(png|jpe?g|svg)$/));
+const folder57 = importAll(require.context('./57', true, /\.(png|jpe?g|svg)$/));
+const folder58 = importAll(require.context('./58', true, /\.(png|jpe?g|svg)$/));
+const folder59 = importAll(require.context('./59', true, /\.(png|jpe?g|svg)$/));
+const folder60 = importAll(require.context('./60', true, /\.(png|jpe?g|svg)$/));
+const folder61 = importAll(require.context('./61', true, /\.(png|jpe?g|svg)$/));
+const folder62 = importAll(require.context('./62', true, /\.(png|jpe?g|svg)$/));
+const folder63 = importAll(require.context('./63', true, /\.(png|jpe?g|svg)$/));
+const folder64 = importAll(require.context('./64', true, /\.(png|jpe?g|svg)$/));
+const folder65 = importAll(require.context('./65', true, /\.(png|jpe?g|svg)$/));
+const folder66 = importAll(require.context('./66', true, /\.(png|jpe?g|svg)$/));
+const folder67 = importAll(require.context('./67', true, /\.(png|jpe?g|svg)$/));
+const folder68 = importAll(require.context('./68', true, /\.(png|jpe?g|svg)$/));
+const folder69 = importAll(require.context('./69', true, /\.(png|jpe?g|svg)$/));
+const folder70 = importAll(require.context('./70', true, /\.(png|jpe?g|svg)$/));
+const folder71 = importAll(require.context('./71', true, /\.(png|jpe?g|svg)$/));
+const folder72 = importAll(require.context('./72', true, /\.(png|jpe?g|svg)$/));
+const folder73 = importAll(require.context('./73', true, /\.(png|jpe?g|svg)$/));
+const folder74 = importAll(require.context('./74', true, /\.(png|jpe?g|svg)$/));
+const folder75 = importAll(require.context('./75', true, /\.(png|jpe?g|svg)$/));
+const folder76 = importAll(require.context('./76', true, /\.(png|jpe?g|svg)$/));
+const folder77 = importAll(require.context('./77', true, /\.(png|jpe?g|svg)$/));
+const folder78 = importAll(require.context('./78', true, /\.(png|jpe?g|svg)$/));
+const folder79 = importAll(require.context('./79', true, /\.(png|jpe?g|svg)$/));
+const folder80 = importAll(require.context('./80', true, /\.(png|jpe?g|svg)$/));
+const folder81 = importAll(require.context('./81', true, /\.(png|jpe?g|svg)$/));
+const folder82 = importAll(require.context('./82', true, /\.(png|jpe?g|svg)$/));
+const folder83 = importAll(require.context('./83', true, /\.(png|jpe?g|svg)$/));
+const folder84 = importAll(require.context('./84', true, /\.(png|jpe?g|svg)$/));
+const folder85 = importAll(require.context('./85', true, /\.(png|jpe?g|svg)$/));
+const folder86 = importAll(require.context('./86', true, /\.(png|jpe?g|svg)$/));
+const folder87 = importAll(require.context('./87', true, /\.(png|jpe?g|svg)$/));
+const folder88 = importAll(require.context('./88', true, /\.(png|jpe?g|svg)$/));
+
+export default {
+	1: folder1,
+	2: folder2,
+	3: folder3,
+	4: folder4,
+	5: folder5,
+	6: folder6,
+	7: folder7,
+	8: folder8,
+	9: folder9,
+	10: folder10,
+	11: folder11,
+	12: folder12,
+	13: folder13,
+	14: folder14,
+	15: folder15,
+	16: folder16,
+	17: folder17,
+	18: folder18,
+	19: folder19,
+	20: folder20,
+	21: folder21,
+	22: folder22,
+	23: folder23,
+	24: folder24,
+	25: folder25,
+	26: folder26,
+	27: folder27,
+	28: folder28,
+	29: folder29,
+	30: folder30,
+	31: folder31,
+	32: folder32,
+	33: folder33,
+	34: folder34,
+	35: folder35,
+	36: folder36,
+	37: folder37,
+	38: folder38,
+	39: folder39,
+	40: folder40,
+	41: folder41,
+	42: folder42,
+	43: folder43,
+	44: folder44,
+	45: folder45,
+	46: folder46,
+	47: folder47,
+	48: folder48,
+	49: folder49,
+	50: folder50,
+	51: folder51,
+	52: folder52,
+	53: folder53,
+	54: folder54,
+	55: folder55,
+	56: folder56,
+	57: folder57,
+	58: folder58,
+	59: folder59,
+	60: folder60,
+	61: folder61,
+	62: folder62,
+	63: folder63,
+	64: folder64,
+	65: folder65,
+	66: folder66,
+	67: folder67,
+	68: folder68,
+	69: folder69,
+	70: folder70,
+	71: folder71,
+	72: folder72,
+	73: folder73,
+	74: folder74,
+	75: folder75,
+	76: folder76,
+	77: folder77,
+	78: folder78,
+	79: folder79,
+	80: folder80,
+	81: folder81,
+	82: folder82,
+	83: folder83,
+	84: folder84,
+	85: folder85,
+	86: folder86,
+	87: folder87,
+	88: folder88
+}
